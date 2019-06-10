102b3d09d0c8d08b55d648fd4217d073beed1310
Andrey <i3hanesy@yandex.ru>
2019-06-10 21:47:29 +0500

    Задание второе

--- js/stat.js
+++ js/stat.js
@@ -17,7 +17,7 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -31,9 +31,10 @@ var getMaxElement = function(arr) {
 
 
 window.renderStatistics = function (ctx, players, times) {
-// Вызов тени облака результатов
+  // Вызов тени облака результатов
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-// Вызов Облака результатов
+
+  // Вызов Облака результатов
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
   ctx.fillStyle = '#000000';
@@ -44,14 +45,18 @@ window.renderStatistics = function (ctx, players, times) {
 
   var maxTime = getMaxElement(times);
 
-  for (var i = 0; i < times.length; i++) {
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BETWEEN_WIDTH - (barHeight * times[i] / maxTime) - TIMES_Y);
-  };
-
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000000';
     ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BETWEEN_WIDTH);
-    (players[i] === 'Вы') ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(30, 30, 150,' + Math.random() + ')';
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BETWEEN_WIDTH - (barHeight * times[i] / maxTime) - TIMES_Y);
+
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+
+      ctx.fillStyle = 'rgba(30, 30, 150,' + Math.random() + ')';
+    }
+
     ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BAR_WIDTH, BAR_WIDTH, -(barHeight * times[i] / maxTime));
-  };
+  }
 };
