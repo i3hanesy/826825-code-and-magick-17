1ea27d03ebc0c986d46a38819dcce7cc79d90d44
Andrey <i3hanesy@yandex.ru>
2019-06-12 16:23:32 +0500

    подсократил код

--- js/stat.js
+++ js/stat.js
@@ -9,7 +9,9 @@ var TIMES_Y = 30;
 var BETWEEN_WIDTH = 50;
 var BAR_WIDTH = 40;
 var barHeight = CLOUD_HEIGHT - 2 * BETWEEN_WIDTH - TIMES_Y;
-
+var barX = CLOUD_X + BAR_WIDTH;
+var additionBarX = BAR_WIDTH + BETWEEN_WIDTH;
+var barY = CLOUD_X * 2 + BETWEEN_WIDTH;
 
 // Создает облако результатов
 var renderCloud = function (ctx, x, y, color) {
@@ -18,24 +20,15 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
+  return Math.max.apply(Math, arr);
 };
-
 
 window.renderStatistics = function (ctx, players, times) {
   // Вызов тени облака результатов
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
 
   // Вызов Облака результатов
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
 
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
@@ -45,10 +38,12 @@ window.renderStatistics = function (ctx, players, times) {
 
   var maxTime = getMaxElement(times);
 
+  var timeHeightBar = barHeight / maxTime;
+
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000000';
-    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BETWEEN_WIDTH);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BETWEEN_WIDTH - (barHeight * times[i] / maxTime) - TIMES_Y);
+    ctx.fillText(players[i], barX + additionBarX * i, barY);
+    ctx.fillText(Math.round(times[i]), barX + additionBarX * i, barY - timeHeightBar * times[i] - TIMES_Y);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -57,6 +52,6 @@ window.renderStatistics = function (ctx, players, times) {
       ctx.fillStyle = 'rgba(30, 30, 150,' + Math.random() + ')';
     }
 
-    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BETWEEN_WIDTH) * i, CLOUD_X * 2 + BAR_WIDTH, BAR_WIDTH, -(barHeight * times[i] / maxTime));
+    ctx.fillRect(barX + additionBarX * i, CLOUD_X * 2 + BAR_WIDTH, BAR_WIDTH, - timeHeightBar * times[i]);
   }
 };
